1b543aa78b97b3d457a1eb5c18c0f8bbeb625c45
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2024-04-29 22:17:46 +0300

    mapper added

--- src/main/java/com/kadiraksoy/springboottodoapi/mapper/TodoMapper.java
+++ src/main/java/com/kadiraksoy/springboottodoapi/mapper/TodoMapper.java
@@ -1,4 +1,21 @@
 package com.kadiraksoy.springboottodoapi.mapper;
 
-public class TodoMapper {
+import com.kadiraksoy.springboottodoapi.model.Todo;
+import com.kadiraksoy.springboottodoapi.payload.payload.TodoResponseDTO;
+import com.kadiraksoy.springboottodoapi.payload.request.TodoRequestDTO;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface TodoMapper {
+
+    @Mapping(target = "completed", ignore = true)
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Todo todoRequestToTodo(TodoRequestDTO todoRequestDTO);
+
+    TodoResponseDTO todoToTodoResponse(Todo todo);
+
+    TodoRequestDTO todoToTodoRequest(Todo todo);
 }
